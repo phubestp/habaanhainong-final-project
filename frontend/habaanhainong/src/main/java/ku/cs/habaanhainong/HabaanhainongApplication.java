c01794191c6d99ba518becaf5c681f92395edea5
phubestp <phubest.p@ku.th>
2023-10-12 02:15:55 +0700

    fixed src not found

---
+++ frontend/habaanhainong/src/main/java/ku/cs/habaanhainong/HabaanhainongApplication.java
@@ -0,0 +1,13 @@
+package ku.cs.habaanhainong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabaanhainongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabaanhainongApplication.class, args);
+    }
+
+}
---
+++ frontend/habaanhainong/src/test/java/ku/cs/habaanhainong/HabaanhainongApplicationTests.java
@@ -0,0 +1,13 @@
+package ku.cs.habaanhainong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabaanhainongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
