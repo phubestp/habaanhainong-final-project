71b784c127bd7a7abc89b48a189612e98a06b970
phubestp <phubest.p@ku.th>
2023-11-03 19:30:40 +0700

    use animals api in home

--- frontend/habaanhainong/src/main/java/ku/cs/habaanhainong/controller/HomeController.java
+++ frontend/habaanhainong/src/main/java/ku/cs/habaanhainong/controller/HomeController.java
@@ -1,14 +1,20 @@
 package ku.cs.habaanhainong.controller;
 
+import ku.cs.habaanhainong.service.AnimalService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+
 @Controller
 public class HomeController {
 
+    private final AnimalService animalService = new AnimalService();
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
+        ArrayList<Object> animals = (ArrayList<Object>) animalService.getAnimals();
+        model.addAttribute("animals",animals.subList(0, 3));
         return "home";
     }
 }
