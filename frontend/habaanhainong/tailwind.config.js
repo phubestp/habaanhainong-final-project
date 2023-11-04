c55b59eff0954feedc194071d66f8536e1603f84
xMickeyS <pichaya.sen@ku.th>
2023-11-04 20:11:34 +0700

    add delete account template

--- frontend/habaanhainong/tailwind.config.js
+++ frontend/habaanhainong/tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
       'secondary': '#FFB000',
       'background': '#FAF8F1',
       'white': '#FFFFFF',
-      'red' : '#F32013'
+      'red' : '#F32013',
+      'yellow' : '#FFB000'
     },
     fontFamily : {
       sans: ['Mitr', 'sans-serif']
