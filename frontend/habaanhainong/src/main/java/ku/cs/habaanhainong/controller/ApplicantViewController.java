ebe2af835b4778c56a539dfafa40e92d0cee48eb
xMickeyS <pichaya.sen@ku.th>
2023-11-05 23:35:57 +0700

    GET all manageable post

--- frontend/habaanhainong/src/main/java/ku/cs/habaanhainong/controller/ApplicantViewController.java
+++ frontend/habaanhainong/src/main/java/ku/cs/habaanhainong/controller/ApplicantViewController.java
@@ -5,11 +5,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 public class ApplicantViewController {
+
     @RequestMapping("/applicant-view")
     public String getApplicantPage(Model model){
+        ArrayList<HashMap<String,String>> applicant = new ArrayList<>();
+        for (int i = 0; i < 2; i++){
+            HashMap<String,String> info = new HashMap<>();
+            info.put("name", "น้องแมวซุกซน");
+            info.put("reason", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
+            info.put("tel", "0000000");
+            info.put("facebook", "abc");
+            info.put("instagram", "def");
+            applicant.add(info);
+        }
+        model.addAttribute("applicants", applicant);
         return "applicant-view";
     }
 }
--- frontend/habaanhainong/src/main/java/ku/cs/habaanhainong/controller/PostManageController.java
+++ frontend/habaanhainong/src/main/java/ku/cs/habaanhainong/controller/PostManageController.java
@@ -1,28 +1,39 @@
 package ku.cs.habaanhainong.controller;
 
+import ku.cs.habaanhainong.service.PostAllService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class PostManageController {
 
+    private final PostAllService postAllService = new PostAllService();
+
     @RequestMapping("/post-manage")
-    public String getPost(Model post){
-        ArrayList<HashMap<String,String>> postList  = new ArrayList<>();
-        for (int i = 0; i < 2; i++){
-            HashMap<String,String> info = new HashMap<>();
-            info.put("name", "น้องแมวซุกซน");
-            info.put("create", "2023-09-23");
-            info.put("header", "หาบ้านให้น้องหน่อยค่ะ เจอน้องอยู่ข้างทางค่ะ");
-            info.put("detail", "เจอน้องแถวๆ มอเกษตรค่ะ มีใครพอจะช่วยเลี้ยงน้องๆได้บ้างมั้ยคะ ฮือ สงสารน้อง");
-            postList.add(info);
-        }
-        post.addAttribute("postList", postList);
+    public String getAllPost(Model model){
+        ArrayList<Object> allPost = (ArrayList<Object>) postAllService.getAllPost();
+        model.addAttribute("posts", allPost);
         return "post-manage";
     }
+
+    @RequestMapping("post-manage/read-post")
+    public String readPost(Model model, @RequestParam Map<String,String> params){
+        String post_id = params.get("post_id");
+        return "redirect:/post-view/" + post_id;
+    }
+
+//    @DeleteMapping("post-manage/delete-post")
+//    public String deletePost(Model model, @RequestParam Map<String,String> params){
+//        System.out.println(params);
+//        String post_id = params.get("post_id");
+//        return "post-manage";
+//    }
 
 }
