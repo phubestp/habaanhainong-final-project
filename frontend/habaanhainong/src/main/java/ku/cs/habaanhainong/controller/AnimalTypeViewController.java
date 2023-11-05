2395c01d68e5731c3619f2c1711179436c5f6de4
xMickeyS <pichaya.sen@ku.th>
2023-11-05 15:04:23 +0700

    waiting for api service for merging

--- frontend/habaanhainong/src/main/java/ku/cs/habaanhainong/controller/AnimalTypeViewController.java
+++ frontend/habaanhainong/src/main/java/ku/cs/habaanhainong/controller/AnimalTypeViewController.java
@@ -2,8 +2,12 @@ package ku.cs.habaanhainong.controller;
 
 import ku.cs.habaanhainong.service.AnimalService;
 import ku.cs.habaanhainong.service.AnimalTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -13,10 +17,16 @@ public class AnimalTypeViewController {
 
     private final AnimalTypeService animalTypeService = new AnimalTypeService();
 
-    @RequestMapping("/animal-type-view")
+    @GetMapping("/animal-type-view")
     public String getAnimalType(Model animalTypes){
         ArrayList<Object> animalTypeLists = (ArrayList<Object>) animalTypeService.getAnimalTypes();
         animalTypes.addAttribute("animalTypes", animalTypeLists);
         return "animal-type-view";
+    }
+
+    @PostMapping("/animal-type-view")
+    public String createMenu(@ModelAttribute MenuRequest menu, Model model) {
+        menuService.createMenu(menu);
+        return "redirect:/menus"
     }
 }
--- frontend/habaanhainong/src/main/java/ku/cs/habaanhainong/service/AnimalTypeService.java
+++ frontend/habaanhainong/src/main/java/ku/cs/habaanhainong/service/AnimalTypeService.java
@@ -1,13 +1,22 @@
 package ku.cs.habaanhainong.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class AnimalTypeService {
-
     public Object getAnimalTypes() {
         RestTemplate restTemplate = new RestTemplate();
         String resourceUrl = "https://habaanhainong-final-project.vercel.app/api/api/animals-type";
         return restTemplate.getForObject(resourceUrl, Object.class);
     }
+
+    public void postAnimalType(){
+
+    }
+
+
+
 
 }
