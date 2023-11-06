671bc357ce286bbf525f4f090cfdab4bd67a5e25
OteEnded <ratnaritjumnong@gmail.com>
2023-11-06 17:39:40 +0700

    tring to make upload picture service

--- frontend/habaanhainong/src/main/java/ku/cs/habaanhainong/testing_area/Test1_APIConnect.java
+++ frontend/habaanhainong/src/main/java/ku/cs/habaanhainong/testing_area/Test1_APIConnect.java
@@ -4,6 +4,9 @@ import ku.cs.habaanhainong.service.APIServices;
 import ku.cs.habaanhainong.service.AnimalsAPI;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
@@ -20,8 +23,19 @@ public class Test1_APIConnect {
 //            System.out.println(animal);
 //        }
 
+//        RestTemplate restTemplate = new RestTemplate();
+//        String resourceUrl = APIServices.BASE_URL + "animals";
+//        System.out.println(restTemplate.getForObject(resourceUrl, Object.class));
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("image_file", "Ote'sImageFile");
+        jsonObject.put("file_extension", "Ote'sFileExtension");
+        String apiUrl = "http://localhost/api/image";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> req = new HttpEntity<>(jsonObject.toString(), headers);
+
         RestTemplate restTemplate = new RestTemplate();
-        String resourceUrl = APIServices.BASE_URL + "animals";
-        System.out.println(restTemplate.getForObject(resourceUrl, Object.class));
+        System.out.println(restTemplate.postForObject(apiUrl, req, Object.class));
     }
 }
